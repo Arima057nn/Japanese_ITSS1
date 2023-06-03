40ba2d4fe3e9b2d3619e467999258e8830d7f4f5
maiquynh051821 <maithuyquynh051821@gmail.com>
2023-06-04 00:06:18 +0700

    update header

--- Frontend/src/components/common/Header/index.js
+++ Frontend/src/components/common/Header/index.js
@@ -4,6 +4,23 @@ import styles from "./Header.module.scss";
 const cx = classNames.bind(styles);
 
 function Header() {
-  return <h1 className={cx("wrapper")}>Header</h1>;
+  return (
+    <>
+      <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+      </head>
+      <div className={cx("wrapper")}>
+        <nav>
+          <h1 className={cx("logo_vlearn")}>VLEARN</h1>
+          <i className={cx("fa fa-bell")}></i>
+          <a href="/home" className={cx("header_img")}>
+            <img src="https://d1hjkbq40fs2x4.cloudfront.net/2017-08-21/files/landscape-photography_1645.jpg"
+              alt="avatar" className={cx("header_avatar")}/>
+          </a>
+          <span className={cx("header-chevron-down")}><i className={cx("fa fa-chevron-down")}></i></span>
+        </nav>
+      </div>
+    </>
+  );
 }
 export default Header;
