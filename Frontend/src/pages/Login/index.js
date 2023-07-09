e55db89630f7164004dd7e1f90fab198b9b93c8e
Arima057nn <tjendung97pain@gmail.com>
2023-07-09 07:51:54 +0700

    [7] fix ui

--- Frontend/src/pages/Login/index.js
+++ Frontend/src/pages/Login/index.js
@@ -10,6 +10,7 @@ import KeyIcon from "@mui/icons-material/Key";
 import PersonIcon from "@mui/icons-material/Person";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import DefaultLayout from "../../components/Layouts/DefaultLayout";
 
 const cx = classNames.bind(styles);
 function Login() {
@@ -62,60 +63,84 @@ function Login() {
     }
   };
   return (
-    <div className={cx("wrapper")}>
-      <div className={cx("form")}>
-        <h1 className={cx("header")} style={{ fontWeight: 600, fontSize: 48 }}>
-          ログイン
-        </h1>
-        <form onSubmit={handleLogin}>
-          <div className={cx("input-container")}>
-            <label style={{ fontSize: 20 }}>ユーザー名 </label>
-            <input
-              type="text"
-              className={cx("input")}
-              required
-              value={user_name}
-              onChange={(e) => setUser_name(e.target.value)}
-            />
-            <div className={cx("icon")}>
-              <PersonIcon />
+    <div style={{ minHeight: "100vh", backgroundColor: "var(--background)" }}>
+      <DefaultLayout
+        content={
+          <div className={cx("wrapper")}>
+            <div className={cx("form")}>
+              <h1
+                className={cx("header")}
+                style={{ fontWeight: 600, fontSize: 48 }}
+              >
+                ログイン
+              </h1>
+              <form onSubmit={handleLogin}>
+                <div className={cx("input-container")}>
+                  <label style={{ fontSize: 20 }}>ユーザー名 </label>
+                  <input
+                    type="text"
+                    className={cx("input")}
+                    required
+                    value={user_name}
+                    onChange={(e) => setUser_name(e.target.value)}
+                  />
+                  <div className={cx("icon")}>
+                    <PersonIcon />
+                  </div>
+                </div>
+
+                <div className={cx("input-container")}>
+                  <label style={{ fontSize: 20 }}>パスワード </label>
+                  <input
+                    type={!isShowPassword ? "password" : "text"}
+                    className={cx("input")}
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <div className={cx("icon")}>
+                    <KeyIcon />
+                  </div>
+
+                  <div
+                    className={cx("iconEye")}
+                    onClick={() => setIsShowPassword(!isShowPassword)}
+                  >
+                    {isShowPassword ? (
+                      <RemoveRedEyeIcon />
+                    ) : (
+                      <VisibilityOffIcon />
+                    )}
+                  </div>
+                </div>
+
+                <div>
+                  <input
+                    className={cx("input-form")}
+                    type="submit"
+                    value="ログイン"
+                    style={{
+                      fontWeight: 700,
+                      fontSize: 20,
+                      height: 57,
+                    }}
+                  />
+                </div>
+              </form>
+
+              <div style={{ marginTop: 40, fontSize: 16 }}>
+                アカウントをお持ちでない方は
+                <Link
+                  style={{ textDecoration: "none", fontWeight: 600 }}
+                  to="/signup"
+                >
+                  こちら
+                </Link>
+              </div>
             </div>
           </div>
-
-          <div className={cx("input-container")}>
-            <label style={{ fontSize: 20 }}>パスワード </label>
-            <input
-              type={!isShowPassword ? "password" : "text"}
-              className={cx("input")}
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <div className={cx("icon")}>
-              <KeyIcon />
-            </div>
-
-            <div
-              className={cx("iconEye")}
-              onClick={() => setIsShowPassword(!isShowPassword)}
-            >
-              {isShowPassword ? <RemoveRedEyeIcon /> : <VisibilityOffIcon />}
-            </div>
-          </div>
-
-          <div>
-            <input
-              className={cx("input-form")}
-              type="submit"
-              value="ログイン"
-            />
-          </div>
-        </form>
-
-        <div style={{ marginTop: 20, fontSize: 20 }}>
-          <Link to="/signup">登録</Link>
-        </div>
-      </div>
+        }
+      ></DefaultLayout>
     </div>
   );
 }
--- Frontend/src/pages/SignUp/index.js
+++ Frontend/src/pages/SignUp/index.js
@@ -5,6 +5,11 @@ import { userApi } from "../../services/user-api";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
+import KeyIcon from "@mui/icons-material/Key";
+import PersonIcon from "@mui/icons-material/Person";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import DefaultLayout from "../../components/Layouts/DefaultLayout";
 
 const cx = classNames.bind(styles);
 
@@ -20,6 +25,8 @@ function SignUp() {
   });
   const [errorMessage, setErrorMessage] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const [isShowCfPassword, setIsShowCfPassword] = useState(false);
 
   const handleRegister = async (event) => {
     event.preventDefault();
@@ -43,75 +50,128 @@ function SignUp() {
   };
 
   return (
-    <div className={cx("wrapper")}>
-      <div className={cx("form")}>
-        <h1 className={cx("header")} style={{ fontWeight: 600, fontSize: 48 }}>
-          登録
-        </h1>
-        <form onSubmit={handleRegister}>
-          <div className={cx("input-container")}>
-            <label style={{ fontSize: 20 }}>ユーザー名</label>
-            <input
-              type="text"
-              className={cx("input")}
-              required
-              value={account.user_name}
-              onChange={(e) =>
-                setAccount({ ...account, user_name: e.target.value })
-              }
-            ></input>
-          </div>
-
-          <div className={cx("input-container")}>
-            <label style={{ fontSize: 20 }}>パスワード</label>
-            <input
-              type="password"
-              className={cx("input")}
-              required
-              value={account.password}
-              onChange={(e) =>
-                setAccount({ ...account, password: e.target.value })
-              }
-            ></input>
-          </div>
-
-          <div className={cx("input-container")}>
-            <label style={{ fontSize: 20 }}>パスワードを認証する</label>
-            <input
-              type="password"
-              className={cx("input")}
-              required
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            ></input>
-          </div>
-          {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
-          <div className={cx("input-container")}>
-            <label style={{ fontSize: 20 }}>役割 </label>
-            <select
-              required
-              className={cx("input")}
-              onChange={(e) => {
-                setAccount({
-                  ...account,
-                  roleId: parseInt(e.target.value),
-                });
-              }}
+    <DefaultLayout
+      content={
+        <div className={cx("wrapper")}>
+          <div className={cx("form")}>
+            <h1
+              className={cx("header")}
+              style={{ fontWeight: 600, fontSize: 48 }}
             >
-              <option value={TEACHER}> 教師</option>
-              <option value={STUDENT}> 学生</option>
-            </select>
-          </div>
+              登録
+            </h1>
+            <form onSubmit={handleRegister}>
+              <div className={cx("input-container")}>
+                <label style={{ fontSize: 20 }}>ユーザー名</label>
+                <input
+                  type="text"
+                  className={cx("input")}
+                  required
+                  value={account.user_name}
+                  onChange={(e) =>
+                    setAccount({ ...account, user_name: e.target.value })
+                  }
+                ></input>
+                <div className={cx("icon")}>
+                  <PersonIcon />
+                </div>
+              </div>
 
-          <div>
-            <input className={cx("input-form")} type="submit" value="登録" />
+              <div className={cx("input-container")}>
+                <label style={{ fontSize: 20 }}>パスワード</label>
+                <input
+                  type={!isShowPassword ? "password" : "text"}
+                  className={cx("input")}
+                  required
+                  value={account.password}
+                  onChange={(e) =>
+                    setAccount({ ...account, password: e.target.value })
+                  }
+                ></input>
+                <div className={cx("icon")}>
+                  <KeyIcon />
+                </div>
+                <div
+                  className={cx("iconEye")}
+                  onClick={() => setIsShowPassword(!isShowPassword)}
+                >
+                  {isShowPassword ? (
+                    <RemoveRedEyeIcon />
+                  ) : (
+                    <VisibilityOffIcon />
+                  )}
+                </div>
+              </div>
+
+              <div className={cx("input-container")}>
+                <label style={{ fontSize: 20 }}>パスワードを認証する</label>
+                <input
+                  type={!isShowCfPassword ? "password" : "text"}
+                  className={cx("input")}
+                  required
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                ></input>
+                <div className={cx("icon")}>
+                  <KeyIcon />
+                </div>
+                <div
+                  className={cx("iconEye")}
+                  onClick={() => setIsShowCfPassword(!isShowCfPassword)}
+                >
+                  {isShowCfPassword ? (
+                    <RemoveRedEyeIcon />
+                  ) : (
+                    <VisibilityOffIcon />
+                  )}
+                </div>
+              </div>
+              {errorMessage && (
+                <div style={{ color: "red" }}>{errorMessage}</div>
+              )}
+              <div className={cx("input-container")}>
+                <label style={{ fontSize: 20 }}>役割 </label>
+                <select
+                  required
+                  className={cx("input")}
+                  onChange={(e) => {
+                    setAccount({
+                      ...account,
+                      roleId: parseInt(e.target.value),
+                    });
+                  }}
+                >
+                  <option value={TEACHER}> 教師</option>
+                  <option value={STUDENT}> 学生</option>
+                </select>
+              </div>
+
+              <div>
+                <input
+                  className={cx("input-form")}
+                  type="submit"
+                  value="登録"
+                  style={{
+                    fontWeight: 700,
+                    fontSize: 20,
+                    height: 57,
+                  }}
+                />
+              </div>
+            </form>
+            <div style={{ fontSize: 16, marginTop: 40 }}>
+              アカウントをお持ちですか？
+              <Link
+                style={{ textDecoration: "none", fontWeight: 600 }}
+                to="/login"
+              >
+                こちら
+              </Link>
+            </div>
           </div>
-        </form>
-        <div style={{ marginTop: 20, fontSize: 20 }}>
-          <Link to="/login">ログイン</Link>
         </div>
-      </div>
-    </div>
+      }
+    ></DefaultLayout>
   );
 }
 
